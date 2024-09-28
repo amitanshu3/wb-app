f6b2d5d0b926f131e1e30f9142c08302a71ed8fc
Amitanshu Behura <144086900+amitanshu3@users.noreply.github.com>
2024-09-28 14:56:40 +0530

    added responsiveness

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -4,7 +4,7 @@ import Layout from '../../components/Layout';
 export default function Dashboard() {
   return (
     <Layout>
-   <div className="  p-8">
+   <div className="  p-8 max-sm:mt-4">
       <h1 className="text-3xl font-bold text-blue-700 mb-4">Welcome to the Dashboard</h1>
       <p className="text-lg text-gray-600">This is your dashboard. More features coming soon!</p>
     </div>
--- src/app/internship/page.jsx
+++ src/app/internship/page.jsx
@@ -4,7 +4,7 @@ import Layout from "../../components/Layout";
 export default function Internship() {
   return (
     <Layout>
-      <div className="p-8">
+      <div className="p-8 max-sm:mt-12">
         <h1 className="text-3xl font-bold text-blue-700 mb-4">
           Internship Opportunities
         </h1>
